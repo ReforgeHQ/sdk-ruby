2233681ede51cb51c8acc3f15d43d6822617f961
James Kebinger <james.kebinger@prefab.cloud>
2025-09-19 17:07:22 -0500

    Update messge to match expectation

--- lib/reforge/errors/initialization_timeout_error.rb
+++ lib/reforge/errors/initialization_timeout_error.rb
@@ -4,7 +4,7 @@ module Reforge
   module Errors
     class InitializationTimeoutError < Reforge::Error
       def initialize(timeout_sec, key)
-        message = "Reforge SDK couldn't initialize in #{timeout_sec} second timeout. Trying to fetch key `#{key}`."
+        message = "Reforge couldn't initialize in #{timeout_sec} second timeout. Trying to fetch key `#{key}`."
         super(message)
       end
     end
