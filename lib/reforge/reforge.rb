9e8a4e7bb6b744670bf506d31bd2b008d5c4421a
Jeff Dwyer <jdwyah@gmail.com>
2025-11-03 10:12:27 -0500

    fix this log_filter method to do what it used to

--- lib/reforge/reforge.rb
+++ lib/reforge/reforge.rb
@@ -49,16 +49,13 @@ module Reforge
   end
 
   def self.log_filter
-    unless defined?(SemanticLogger)
-      # SemanticLogger is optional - return a pass-through filter
-      # Only log debug message if explicitly enabled
-      LOG.debug 'log_filter called but SemanticLogger is not available. Install the semantic_logger gem to use this feature.' if ENV['REFORGE_LOG_CLIENT_BOOTSTRAP_LOG_LEVEL'] == 'debug'
-      return Proc.new { |log| true } # Pass through all logs
-    end
-
     InternalLogger.using_reforge_log_filter!
     return Proc.new do |log|
-      bootstrap_log_level(log)
+      if defined?(@singleton) && !@singleton.nil? && @singleton.config_client.initialized?
+        @singleton.log.semantic_filter(log)
+      else
+        bootstrap_log_level(log)
+      end
     end
   end
 
